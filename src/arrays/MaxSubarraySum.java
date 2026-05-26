45828d4ac45f5f174259c14bebde1ea162b4c4d3
ManojNaik2712 <manunaik599@gmail.com>
2026-05-26 12:43:26 +0530

    more problems on arrays

---
+++ src/arrays/MaxSubarraySum.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class MaxSubarraySum {
+    public static int maxSubarraySum(int[] arr){
+
+        int currentSum = 0;
+        int maximumSum = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            currentSum +=arr[i];
+            maximumSum = Math.max(maximumSum,currentSum);
+
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maximumSum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-5,-2,-8};
+
+        System.out.println(maxSubarraySum(arr));
+    }
+}
+
+/* Time : O(N)
+   Space : O(1)
+   Approach : kadens algorithm
+ */
---
+++ src/arrays/StockBuySell.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class StockBuySell {
+    private static int stockBuySell(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            int profit = price - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(stockBuySell(prices));
+    }
+
+}
+
+/*
+  Time : O(N)
+  Space : O(1)
+  pattern : Running minimum pattern
+*/
